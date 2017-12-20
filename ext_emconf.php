d046059905631a0c8f758833461c548452cf5c81
Christopher Kaster <me@atomicptr.de>
2017-12-20 16:37:01 +0100

    raise version to 0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     "title" => "pagespeed+",
     "description" => "Improve your TYPO3 page speed by using common best practices and other shenanigans :).",
     "category" => "misc",
@@ -11,14 +11,14 @@ $EM_CONF[$_EXTKEY] = array(
     "uploadfolder" => "0",
     "createDirs" => "",
     "clearCacheOnLoad" => 1,
-    "version" => "0.2.0",
-    "constraints" => array(
-        "depends" => array(
+    "version" => "0.3.0",
+    "constraints" => [
+        "depends" => [
             "typo3" => "8.7.0-8.7.99",
-        ),
-        "conflicts" => array(
-        ),
-        "suggests" => array(
-        ),
-    ),
-);
+        ],
+        "conflicts" => [
+        ],
+        "suggests" => [
+        ],
+    ],
+];
